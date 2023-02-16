f6104b6c4e9218b1f3fd6ab810752656df4b319d
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-16 12:31:58 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h3>Main Branch</h3>
+    <p>This app was created using a clean bootstrapped react template.</p>
+    <p>The stock react App.js code has been removed.</p>
+    </div>
+  );
+}
+
+export default App;
